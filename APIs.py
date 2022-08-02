151d2affaeeb05875d7212ebd4731296ff7d7513
Comisión de regulación de comunicaciones <95722715+postdatacrc@users.noreply.github.com>
2022-08-02 16:31:37 -0500

    Add files via upload

--- APIs.py
+++ APIs.py
@@ -484,3 +484,23 @@ def ReadApiMExpresaEnviosIngresos():
     MExpresa['tipo_envio']=MExpresa['tipo_envio'].replace({'Envíos Individuales':'Individuales','Envíos Masivos':'Masivos'})
     return MExpresa  
 IngresosyEnviosMExpresa=ReadApiMExpresaEnviosIngresos()
+
+########################################################### Giros
+##Ingresos 
+def ReadApiGirosIngresos():
+    resourceid = 'cbb58f7a-0d33-4667-a8df-8f7a707d0893'
+    consulta_anno='2018,2019,2020,2021'
+    consulta='https://www.postdata.gov.co/api/action/datastore/search.json?resource_id=' + resourceid + ''\
+             '&filters[anno]=' + consulta_anno + ''\
+             '&fields[]=anno&fields[]=trimestre&fields[]=ambito&fields[]=tipo_giro'\
+             '&group_by=anno,trimestre,ambito,tipo_giro'\
+             '&sum[]=ingresos&sum[]=valor_total_giros' 
+    response_base = urlopen(consulta + '&limit=10000000') 
+    json_base = json.loads(response_base.read())
+    Giros = pd.DataFrame(json_base['result']['records'])
+    Giros['periodo']=Giros['anno']+'-T'+Giros['trimestre']
+    Giros.sum_ingresos = Giros.sum_ingresos.astype('int64')
+    Giros.sum_valor_total_giros = Giros.sum_valor_total_giros.astype('int64')
+    Giros = Giros.rename(columns={'sum_valor_total_giros':'Valor total giros','sum_ingresos':'Ingresos'})
+    return Giros  
+IngresosGiros=ReadApiGirosIngresos()
